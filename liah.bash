8f7b8a46d4c084a2259cc602179e488a4f134ce0
a8568730 <a8568730@hotmail.com>
2020-03-10 18:22:17 +0800

    + 04讀文章真趣味 01本

---
+++ liah.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# 04文章1 文
+curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A01(%E9%9F%B3%E6%AA%94)/0401-[0-7][0-9].mp3" -o "mp3/04/01/0401-#1#2.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
+
