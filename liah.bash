14a23809fe3850f82240a16c009935299655ba14
a8568730 <a8568730@hotmail.com>
2020-03-10 18:24:21 +0800

    * 04 讀文章 02 本 文章

--- liah.bash
+++ liah.bash
@@ -2,5 +2,6 @@
 
 
 # 04文章1 文
-curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A01(%E9%9F%B3%E6%AA%94)/0401-[0-7][0-9].mp3" -o "mp3/04/01/0401-#1#2.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
+#curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A01(%E9%9F%B3%E6%AA%94)/0401-[0-7][0-9].mp3" -o "mp3/04/01/0401-#1#2.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
 
+curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A02(%E9%9F%B3%E6%AA%94)/0402-[0-7][0-9].mp3" -o "mp3/04/02/0402-#1#2.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
