b79d212fada6bf566302ad8bd065a49b7ab9b6e3
a8568730 <a8568730@hotmail.com>
2020-03-11 15:01:38 +0800

    * 03例句01本

--- liah.bash
+++ liah.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 
+# 03 例句
+
+curl "http://language.moe.gov.tw/upload/download/jts/03%E8%AA%9E%E5%8F%A51(%E9%9F%B3%E6%AA%94)/0301_[01-16]_[01-99]_[03-99].mp3" -o "mp3/03/01/0301_#1_#2_#3.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
+
+#curl "http://language.moe.gov.tw/upload/download/jts/03%E8%AA%9E%E5%8F%A52(%E9%9F%B3%E6%AA%94)/0302_[01-16]_[01-99]_[03-99].mp3" -o "mp3/03/02/0302_#1_#2_#3.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
+
+
+
 # 04文章 文
 #curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A01(%E9%9F%B3%E6%AA%94)/0401-[0-7][0-9].mp3" -o "mp3/04/01/0401-#1#2.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
 
@@ -14,4 +22,4 @@
 
 
 # 04文章 文法
-curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A0{1,2}(%E9%9F%B3%E6%AA%94)/040{1,2}-[07-99:2]-[A-Z:1].mp3" -o "mp3/04/0#1/040#2-#3-#4.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
+#curl "http://language.moe.gov.tw/upload/download/jts/04%E6%96%87%E7%AB%A0{1,2}(%E9%9F%B3%E6%AA%94)/040{1,2}-[07-99:2]-[A-Z:1].mp3" -o "mp3/04/0#1/040#2-#3-#4.mp3" -fLO --create-dirs --retry 100 --retry-delay 10
