01bd17e28444f7a73868075749ba7632febcd91a
a8568730 <a8568730@hotmail.com>
2020-04-11 20:28:24 +0800

    * bosianntiau.json póo `phuan`

--- 01_unbo.py
+++ 01_unbo.py
@@ -2,7 +2,8 @@ import json
 
 with open('bosianntiau.json') as tong:
 	tsuliau = json.load(tong)
-	for im in tsuliau['無聲調']:
-		print(
-			'curl "http://language.moe.gov.tw/upload/download/jts/01%E6%8B%BC%E9%9F%B3(%E9%9F%B3%E6%AA%94)/0101-{{27}}-{{{}}}.mp3" -o "mp3/01/01/0101-#1-#2.mp3" -fL --create-dirs --retry 100 --retry-delay 10'.format(im)
-		)
+	for iah in range(27, 37):
+		for im in tsuliau['無聲調']:
+			print(
+			'curl "http://language.moe.gov.tw/upload/download/jts/01%E6%8B%BC%E9%9F%B3(%E9%9F%B3%E6%AA%94)/0101-{{{0}}}-{{{1}}}.mp3" -o "mp3/01/01/0101-#1-#2.mp3" -fL --create-dirs --retry 100 --retry-delay 10'.format(iah,im)
+			)
--- 28_29.bash
+++ 28_29.bash
@@ -1,6 +1,7 @@
 
-for i in $(seq 30 30)
-do 
+for i in $(seq 31 31)
+do
+  echo "i=$i" 
   arr=("p" "ph" "m" "b" "t" "th" "n" "l" "k" "kh" "h" "ng" "g" "ts" "tsh" "s" "j")
   for s in "${arr[@]}"
   do
@@ -14,7 +15,11 @@ do
   #p.29
   #arr=("ann" "inn" "enn" "onn" "iann" "iunn" "iaunn" "uann" "uainn")
   #p.30
-  arr=("am" "an" "ang" "im" "in" "ing" "iam")
+  #arr=("am" "an" "ang" "im" "in" "ing" "iam")
+  #p.31
+  arr=("ian" "iang" "iong" "un" "uan" "uang" "om" "ong")
+  #p.32
+  #arr=("ap" "at" "ak" "ah" "ip" "it" "ik")
   for u in "${arr[@]}"
   do
     if [ ! -f "mp3/01/01/0101-"$i"-"$u".mp3" ]
