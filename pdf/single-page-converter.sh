421133f7d830398cbf8abe229558571989ea5bcf
a8568730 <a8568730@hotmail.com>
2020-07-12 14:30:56 +0800

    + 2-1-right-sections.pdf

--- pdf/single-page-converter.sh
+++ pdf/single-page-converter.sh
@@ -2,10 +2,16 @@
 # Solution from https://superuser.com/a/235401
 
 # First step: extract the left sections from each of the input pages
+#gs \
+#    -o 2-1-left-sections.pdf \
+#    -sDEVICE=pdfwrite \
+#    -g4210x5950 \
+#    -c "<</PageOffset [0 0]>> setpagedevice" \
+#    -f 02語詞1.pdf
+
 gs \
-    -o 2-1-left-sections.pdf \
+    -o 2-1-right-sections.pdf \
     -sDEVICE=pdfwrite \
     -g4210x5950 \
-    -c "<</PageOffset [0 0]>> setpagedevice" \
+    -c "<</PageOffset [-421 0]>> setpagedevice" \
     -f 02語詞1.pdf
-
