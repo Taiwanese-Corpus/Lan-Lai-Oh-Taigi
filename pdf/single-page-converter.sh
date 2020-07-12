ce40a66bf915f97051039d2fd70f86325dd990b7
a8568730 <a8568730@hotmail.com>
2020-07-12 14:36:24 +0800

    + 2-1single-pages-output.pdf

--- pdf/single-page-converter.sh
+++ pdf/single-page-converter.sh
@@ -9,9 +9,15 @@
 #    -c "<</PageOffset [0 0]>> setpagedevice" \
 #    -f 02語詞1.pdf
 
-gs \
-    -o 2-1-right-sections.pdf \
-    -sDEVICE=pdfwrite \
-    -g4210x5950 \
-    -c "<</PageOffset [-421 0]>> setpagedevice" \
-    -f 02語詞1.pdf
+#gs \
+#    -o 2-1-right-sections.pdf \
+#    -sDEVICE=pdfwrite \
+#    -g4210x5950 \
+#    -c "<</PageOffset [-421 0]>> setpagedevice" \
+#    -f 02語詞1.pdf
+
+pdftk \
+  A=2-1-left-sections.pdf \
+  B=2-1-right-sections.pdf \
+  shuffle A B \
+  output 2-1single-pages-output.pdf
