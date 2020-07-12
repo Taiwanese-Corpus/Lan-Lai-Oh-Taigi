10caa2bec2c0f29141f8b056297c5e26fcb1f701
a8568730 <a8568730@hotmail.com>
2020-07-12 14:28:27 +0800

    + 2-1-left-sections.pdf, single-page-converter.sh

---
+++ pdf/single-page-converter.sh
@@ -0,0 +1,11 @@
+#/!bin/bash
+# Solution from https://superuser.com/a/235401
+
+# First step: extract the left sections from each of the input pages
+gs \
+    -o 2-1-left-sections.pdf \
+    -sDEVICE=pdfwrite \
+    -g4210x5950 \
+    -c "<</PageOffset [0 0]>> setpagedevice" \
+    -f 02語詞1.pdf
+
